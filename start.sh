c2d6f966d2af26e0e84118a0e0b35b06feb554bd
imorel <imorel.w@gmail.com>
2025-10-09 09:47:08 +0700

    setup deploy

--- start.sh
+++ start.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-# Jalankan sekali saja
-if [ ! -f /var/www/html/.installed ]; then
-    composer install --no-dev --optimize-autoloader
-    php artisan key:generate
-    php artisan migrate
-    touch /var/www/html/.installed
-fi
-
-# Jalankan Laravel
 php artisan serve --host=0.0.0.0 --port=$PORT
